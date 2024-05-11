4afb53f851f7c8cc0d05d0cecf1c4ba4c18f82f8
Carissa Hluchan <carissahluchan@gmail.com>
2024-05-10 19:15:59 -0600

    Finish books and start shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,44 @@
 
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+  shelf.unshift(book);
+  }
+}
+
+
+function unshelfBook(targetTitle, shelf) {
+  // loop through arry
+  for (var i = 0; i < shelf.length; i ++)
+  // check if title matches
+    if (targetTitle === shelf[i].title) {
+      shelf.splice(i, 1)
+    }
+  // remove book
+}
+
+function listTitles(shelf) {
+// make string var
+  var allTitles = '';
+// loop through the array
+  for (var i = 0; i < shelf.length; i ++){
+// access the book, and grab the title
+// var currentTitle = shelf[i].title;
+// add title end of exsiting string
+    allTitles += shelf[i].title;
+// add a , space...except at the end
+    if (i < shelf.length - 1) {
+  // add, space
+    allTitles += ', '
+    }
+  }
+// return the string
+return allTitles;
+}
+
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+  unshelfBook,
+  listTitles,
   // searchShelf
 };
