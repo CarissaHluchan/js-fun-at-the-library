0f98138d2f923fe847a38a670526ebd17b8c924f
Carissa Hluchan <carissahluchan@gmail.com>
2024-05-11 13:00:51 -0600

    Finish shelf and library functions

--- src/library.js
+++ src/library.js
@@ -1,8 +1,81 @@
+function createLibrary(libraryName) {
+var library = {
+  name: libraryName,
+  shelves: {
+    fantasy:[], 
+    fiction:[], 
+    nonFiction:[], 
+  }
+}
+return library;
+}
+
+// function addBook(library, book) {
+//   library.shelves.fantasy.push(book);
+// }
+
+function addBook(library, book) {
+  if (book.genre === "fantasy") {
+    library.shelves.fantasy.push(book);
+  } else if (book.genre === "fiction") {
+      library.shelves.fiction.push(book);
+  } else if (book.genre === "nonFiction") {
+      library.shelves.nonFiction.push(book);
+  } else {}
+}
+
+function checkoutBook(library, title, genre) {
+  var shelf = library.shelves[genre];
+  for (var i = 0; i < shelf.length ; i++){
+    var book = shelf[i];
+    if (book.title === title) {
+      shelf.splice(i,1);
+      return `You have now checked out ${title} from the ${library.name}.`
+    } 
+  }
+  return `Sorry, there are currently no copies of ${title} available at the ${library.name}.`
+}
+
+// function checkoutBook(library, title, genre) {
+// //   // should unshelf a book to check out a book to a patron
+// //   // find the book on the shelf.  Find the shelf first
+//   var shelf = library.shelves[genre];
+//   for (var i = 0; i < shelf.length ; i++){
+// //   // pull it off the shelf .pop or .slice or .splice
+// //     var book = shelf[i];
+//     if (book.title === title) {
+//       shelf.splice(i,1);
+//       return `You have now checked out ${title} from the ${library.name}.`
+//     } 
+// //   // console.log to the patron
+//   }
+//   return `Sorry, there are currently no copies of ${title} available at the ${library.name}.`
+// }
+
+function takeStock(library, genre) {
+// should be able to take stock of how many books are on a shelf
+// find the shelf
+// count the number of elements on the shelf .length
+  if (genre !== undefined) {
+    var elmOnShelf = library.shelves[genre].length
+    return `There are a total of ${elmOnShelf} ${genre} books at the ${library.name}.`
+  } else {
+    var numOFBooks = 0;
+    numOFBooks += library.shelves.fantasy.length; 
+    numOFBooks += library.shelves.fiction.length; 
+    numOFBooks += library.shelves.nonFiction.length; 
+  return `There are a total of ${numOFBooks} books at the ${library.name}.`
+  }
+}
+// should be able to take stock of how many books are in the 
+// entire collection if a specific shelf is not specified
+
+
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook,
-  // takeStock
+  createLibrary,
+  addBook,
+  checkoutBook,
+  takeStock
 };
--- src/shelf.js
+++ src/shelf.js
@@ -1,44 +1,60 @@
 
 function shelfBook(book, shelf) {
   if (shelf.length < 3) {
-  shelf.unshift(book);
+    shelf.unshift(book);
   }
 }
 
 
 function unshelfBook(targetTitle, shelf) {
   // loop through arry
-  for (var i = 0; i < shelf.length; i ++)
-  // check if title matches
+  for (var i = 0; i < shelf.length; i++)
+    // check if title matches
     if (targetTitle === shelf[i].title) {
       shelf.splice(i, 1)
     }
   // remove book
 }
 
+
 function listTitles(shelf) {
-// make string var
+  // make string var
   var allTitles = '';
-// loop through the array
-  for (var i = 0; i < shelf.length; i ++){
-// access the book, and grab the title
-// var currentTitle = shelf[i].title;
-// add title end of exsiting string
+  // loop through the array
+  for (var i = 0; i < shelf.length; i++) {
+    // access the book, and grab the title
+    // var currentTitle = shelf[i].title;
+    // add title end of exsiting string
     allTitles += shelf[i].title;
-// add a , space...except at the end
+    // add a , space...except at the end
     if (i < shelf.length - 1) {
-  // add, space
-    allTitles += ', '
+      // add, space
+      allTitles += ', '
     }
   }
-// return the string
-return allTitles;
+  // return the string
+  return allTitles;
 }
+
+
+function searchShelf(shelf, title) {
+// loop through items on a shelf
+for (var i = 0; i < shelf.length; i++) {
+  // identify if a book is on the self
+  if (shelf[i].title === title) {
+    return true;
+  }
+}
+// return a value of ture if book is on the shelf
+// if the book is not on the shelf return false
+return false;
+}
+
 
 
 module.exports = {
   shelfBook,
   unshelfBook,
   listTitles,
-  // searchShelf
+  searchShelf
 };
